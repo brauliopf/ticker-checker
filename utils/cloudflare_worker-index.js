724fc08f392f9300011c0f9a6de800608aa26f7f
Braulio Fernandes <brauliopf+github@gmail.com>
2024-05-16 16:08:50 -0400

    backup cloudflare worker index and logged instructions to set up worker with npm

---
+++ utils/cloudflare_worker-index.js
@@ -0,0 +1,40 @@
+// create worker:  $npm create cloudflare@latest
+// deploy changes: $npx wrangler deploy
+// create env var: $npx wrangler secret put OPENAI_API_KEY 
+
+import OpenAI from 'openai';
+
+const corsHeaders = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'POST, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type'
+}
+
+export default {
+	async fetch(request, env, ctx) {
+		// Handle CORS preflight requests
+		if (request.method === 'OPTIONS') {
+			return new Response(null, { headers: corsHeaders });
+		}
+
+		const openai = new OpenAI({
+			apiKey: env.OPENAI_API_KEY
+		})
+		const messages = await request.json()
+		
+		try {
+			const chatCompletion = await openai.chat.completions.create({
+                model: 'gpt-4',
+                messages: messages,
+                temperature: 1.1,
+                presence_penalty: 0,
+                frequency_penalty: 0
+            })
+            const response = chatCompletion.choices[0].message
+            
+            return new Response(JSON.stringify(response), { headers: corsHeaders })
+		} catch (error) {
+			return new Response(error, { headers: corsHeaders })
+		}
+	},
+};
