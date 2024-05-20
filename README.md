63b32eb66d10870752b435da9b632bf75209b464
Braulio Fernandes <brauliopf+github@gmail.com>
2024-05-20 14:34:57 -0500

    adds comments to worker invocatio at index.js

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # App: ticker-checker
 Web app to check recent history of stock prices
 
+# Cloudflare Workers
+- create worker:  $npm create cloudflare@latest
+- deploy changes: $npx wrangler deploy
+- create env var: $npx wrangler secret put OPENAI_API_KEY 
+
 # Notes / Dev Log
 * Chrome throws a CORS error because I have an http request from a local machine with the url begining with "file://". To fix that, we use a local node server with "http-server".
 ** $npm install -g http-server
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { dates } from './utils/dates.js'
-import config from './config.js'; // Import the configuration file
-
 const tickersArr = []
 const generateReportBtn = document.querySelector('.btn#generate-report')
 
+// Map UI elements
+const loadingArea = document.querySelector('.loading-panel')
+const apiMessage = document.getElementById('api-message')
+
+// Create js triggers
 generateReportBtn.addEventListener('click', fetchStockData)
 
 document.getElementById('ticker-input-form').addEventListener('submit', (e) => {
@@ -22,7 +25,10 @@ document.getElementById('ticker-input-form').addEventListener('submit', (e) => {
     }
 })
 
+// Auxiliary functions
 function renderTickers() {
+    /*  Reset the ticker display and render the tickers in tickerArr
+    */
     const tickersDiv = document.querySelector('.ticker-choice-display')
     tickersDiv.innerHTML = ''
     tickersArr.forEach((ticker) => {
@@ -33,19 +39,27 @@ function renderTickers() {
     })
 }
 
-const loadingArea = document.querySelector('.loading-panel')
-const apiMessage = document.getElementById('api-message')
-
 async function fetchStockData() {
+    /*  Fetch stock data from Polygon.io for each ticker in tickersArr.
+        Input:
+            . tickersArr: array of strings with the tickers to fetch data for.
+            . dates: object with start and end dates for the data.
+        Output:
+            . stockData: array of strings with the stock data for each ticker.
+            (check polygon.io api for more)
+    */
+
+    const POLYGONIO_WORKER_URL = 'https://polygon-api-worker.brauliopf.workers.dev/'
     document.querySelector('.action-panel').style.display = 'none'
     loadingArea.style.display = 'flex'
+
     try {
         const stockData = await Promise.all(tickersArr.map(async (ticker) => {
-            // GET ticket info from Polygon.io
-            const url = `${config.POLYGONIO_CLOUDFLARE_WORKER}?ticker=${ticker}&startDate=${dates.startDate}&endDate=${dates.endDate}`
+            // Make Request
+            const url = `${POLYGONIO_WORKER_URL}?ticker=${ticker}&startDate=${dates.startDate}&endDate=${dates.endDate}`
             const response = await fetch(url)
-            
-            // Digest output and handle error
+
+            // Handle Response
             if(!response.ok) {
                 const errMsg = await response.text()
                 throw new Error('Worker error: ' + errMsg)
@@ -60,37 +74,37 @@ async function fetchStockData() {
     }
 }
 
+
 async function fetchReport(tickersReport_Polygon) {
 
-    try {
-        const messages = [
-            {"role": "system", "content": "You are a financial advisor hired to help clients diversify their investment portfolio with US stock market investments. \
-            Your clients will bring up to 3 tickers which they are considering. You will be provided complete data from each day of the past 3 trading days, including: \
-                'v': trading volume, 'vw': volume weighted average price, 'o': open price, 'c': close price, \
-                'h': highest price, 'l': lowest price, 't': start timestamp, 'n': number of transactions. \
-            You must provide a yes or no decision regarding an investment in each ticker \
-            and a short reasoning (with no more than 50 words for each) for that decision based on data shared on the stock and any public news available. Do mention \
-            the maximum and minimum price of the stock in the past 3 days and, if is relevant, comment on the historical maxima as well."},
-            {"role": "user", "content": tickersReport_Polygon},
-        ]
+    const messages = [
+        {"role": "system", "content": "You are a financial advisor hired to help clients diversify their investment portfolio with US stock market investments. \
+        Your clients will bring up to 3 tickers which they are considering. You will be provided complete data from each day of the past 3 trading days, including: \
+            'v': trading volume, 'vw': volume weighted average price, 'o': open price, 'c': close price, \
+            'h': highest price, 'l': lowest price, 't': start timestamp, 'n': number of transactions. \
+        You must provide a yes or no decision regarding an investment in each ticker \
+        and a short reasoning (with no more than 50 words for each) for that decision based on data shared on the stock and any public news available. Do mention \
+        the maximum and minimum price of the stock in the past 3 days and, if is relevant, comment on the historical maxima as well."},
+        {"role": "user", "content": tickersReport_Polygon},
+    ]
 
-        const worker_URL = config.OPENAI_CLOUDFLARE_WORKER
-        const options = {
+    try {
+        // Make Request
+        const OPENAI_WORKER_URL = 'https://openai-api-worker.brauliopf.workers.dev/'
+        const response = await fetch(OPENAI_WORKER_URL, {
             method: 'POST',
             header: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(messages) // as defined in the header above, the body must be of type JSON
-        }
+        })
 
-        const response = await fetch(worker_URL, options)
+        // Handle Response
         const response_json = await response.json()
-
-        // handle error
+        loadingArea.style.display = 'none'
         if(!response.ok) {
             throw new Error(`Worker error: ${response_json.error}`)
         }
-        loadingArea.style.display = 'none'
         renderReport(response_json.content)
     } catch(error) {
         console.error('There was a problem with the fetch operation –', error);
